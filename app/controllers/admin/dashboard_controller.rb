2bfa985720fcc0d4efa8060fea38d06dc71d5f1e
bainjen <jennifer.fleming.bain@gmail.com>
2020-10-15 00:27:00 +0000

    update authenitcation for admins

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,12 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: "Jungle", password: "book"
+  # http_basic_authenticate_with name: "Jungle", password: "book"
+  # http_basic_authenticate_with :name => ENV['ADMIN_USER'], :password => ENV['ADMIN_PASS']
+  http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PASS']
 
   def show
   end
 end
+
+
+# :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
+# :secret_key      => ENV['STRIPE_SECRET_KEY']
