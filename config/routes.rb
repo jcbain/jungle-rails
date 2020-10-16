eb85060b2492f1258ee34de694bfaeca0084c524
bainjen <jennifer.fleming.bain@gmail.com>
2020-10-16 02:13:05 +0000

    move admin routes to bottom of routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -12,22 +12,22 @@ Rails.application.routes.draw do
 
   resources :orders, only: [:create, :show]
 
+  
+  get '/about' => 'about#show'
+  
+  # these routes are for showing users a login form, logging them in, and logging them out.
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
     resources :categories, only: [:index, :new, :create]
   end
-
-  get '/about' => 'about#show'
-
-  # these routes are for showing users a login form, logging them in, and logging them out.
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  get '/logout' => 'sessions#destroy'
-
-  get '/signup' => 'users#new'
-  post '/users' => 'users#create'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
