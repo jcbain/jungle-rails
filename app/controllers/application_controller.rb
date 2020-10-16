0b751348fcd6bb9117fd418136e54df1a5cc0f1b
bainjen <jennifer.fleming.bain@gmail.com>
2020-10-16 02:35:11 +0000

    move authetication to private in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  private
+  
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
@@ -11,13 +14,11 @@ class ApplicationController < ActionController::Base
     redirect_to '/login' unless current_user
   end
   
-  private
-
   def cart
     @cart ||= cookies[:cart].present? ? JSON.parse(cookies[:cart]) : {}
   end
   helper_method :cart
-
+  
   def enhanced_cart
     @enhanced_cart ||= Product.where(id: cart.keys).map {|product| { product:product, quantity: cart[product.id.to_s] } }
   end
